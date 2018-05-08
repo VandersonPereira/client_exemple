9355f9618191975d4ae905422771a362997527a4
Vanderson Pereira <vandoap@hotmail.com>
2018-05-08 14:31:36 -0300

    Start integration. Create json.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-//alert('Teste de javascript');
+$(document).ready(function(){
+    $("#enviar").click(function(){
+        
+        //Create object
+        var item = new Object();
+        item.name = $('#name').val();
+        item.ativo = true;
+
+        $ajax({
+            url: 'http://localhost:5000/api/todo/',
+            type: 'POST',
+            dataType: 'json',
+            data: item,
+            success: function(data, textStatus, xhr){
+                alert(data);
+            },
+            error: function(xhr, textStatus, errorThrown){
+                alert('Error in Operation');
+            }
+        });
+    });
+});
